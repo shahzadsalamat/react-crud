b3018cf5dedc478dfcccf7fb33a19d5269dc6794
shahzadsalamat <shahzad.salamat@yahoo.com>
2020-03-16 23:20:32 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Live Demo:
-
-https://shahzadsalamat.github.io/react-crud/
---
+++ precache-manifest.9850ed6667ef806aae3f26099f5f83bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf02e294e7b914e4e96c9bb4ccc121e5",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "d2644fc6f04fc66822d3",
+    "url": "/react-crud/static/css/main.3941a61b.chunk.css"
+  },
+  {
+    "revision": "66db9006e535d71101c3",
+    "url": "/react-crud/static/js/2.e6c5a191.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-crud/static/js/2.e6c5a191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2644fc6f04fc66822d3",
+    "url": "/react-crud/static/js/main.554824c5.chunk.js"
+  },
+  {
+    "revision": "aefd29e02fb5b9f512ce",
+    "url": "/react-crud/static/js/runtime-main.e8c09e10.js"
+  }
+]);
