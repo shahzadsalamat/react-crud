32a87dd2490d54aed984c1d366564912595678ca
Shahzad Salamat <shahzad.salamat@relexsolutions.com>
2022-02-13 15:33:02 +0200

    delete shahzad email

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
                       )}
             </table>
               <p>this is shahzad changes</p>
-              <p>shahzad.salamat@yahoo.com</p>
+              <p>samson.salamat@yahoo.com</p>
         </div>
       </div>
     );
